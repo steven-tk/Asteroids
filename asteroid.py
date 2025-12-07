a518d0048873925a8388351fd2dc634b20890339
Steven <steven.tk@me.com>
2025-12-07 21:53:43 +0100

    added asteroid bounce

--- asteroid.py
+++ asteroid.py
@@ -42,12 +42,6 @@ class Asteroid(CircleShape):
             new2.velocity = self.velocity.rotate(-random_angle)
     
     def bounce(self, other):
-        # probably need to define contact plane first and then bounce off of that?
-        self.velocity = self.velocity.rotate(45)
-        other.velocity = other.velocity.rotate(45)
-
-
-    def bounce(self, other):
         # push each half the overlap distance apart
         delta = other.position - self.position
         dist = delta.length()
